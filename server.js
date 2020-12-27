0a5a15d50b8e512b321f200e6922d2367ca0fe59
Divana13 <dintccd@gmail.com>
2020-12-27 22:08:55 +0700

    npm

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // set header content type
-  res.setHeader("Content-Type", "text/plain");
-  res.write("hello, ninja");
-  res.end();
+  res.setHeader("Content-Type", "text/html");
+  // set html to browser
+  let path = './view/';
+
+  switch(req.url){
+    case '/':
+      path += 'index.html';
+      res.statusCode=200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode=200;
+      break;
+    case '/about_me':
+      res.statusCode=301;
+      res.setHeader('Location','/about');
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode=404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) =>{
+    if (err){
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+
 });
 
 server.listen(3000, "localhost", () => {
