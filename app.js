a736345e4f41bbbfbe8ae674437609dbd0fedc43
Dint <dintccd@gmail.com>
2020-12-28 17:42:40 +0700

    static

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.set("view engine", "ejs");
 //listening to port
 
 app.listen(3000);
+
+// middleware & static files
+
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
