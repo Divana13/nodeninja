8c62e550ca397573dc5d4838d876645ad6aa7cc4
Divana13 <dintccd@gmail.com>
2020-12-28 22:37:07 +0700

    mongodb

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to MongoDb
+const dbURI = 'mongodb+srv://dint:dint395799@nodeninja.ovbsv.mongodb.net/node-ninja?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(result =>{console.log('connected to db')})
+    .catch(err =>{console.log(err)});
 
 //register view engine
 app.set("view engine", "ejs");
